a5359f4f0f868eb688e1242b7f645125621659d7
Vishal Kumar <gamingvk094@gmail.com>
2025-01-29 16:43:17 +0530

    Update Firebase.js

--- backend/src/models/Firebase.js
+++ backend/src/models/Firebase.js
@@ -3,12 +3,7 @@ const { getFirestore } = require('firebase/firestore');
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBak-OP0C8GDS29e_eUfVJt6ugJnYa7phQ",
-  authDomain: "tweenty-question.firebaseapp.com",
-  projectId: "tweenty-question",
-  storageBucket: "tweenty-question.firebasestorage.app",
-  messagingSenderId: "819108968599",
-  appId: "1:819108968599:web:ff53dbcb2925aa347211d2",
+  
 };
 
 // Initialize Firebase
